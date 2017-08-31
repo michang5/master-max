9c4a09b74b0ffbe0106f8f8673017eeab1aa0609
JoeAngel <joe10330@gmail.com>
2017-08-31 15:11:29 +0800

    Fix conflict

--- js/script.js
+++ js/script.js
@@ -1,46 +1,4 @@
 $(function () {
-    $(document).ready(function () {
-        var slider = $('#lightSlider').lightSlider({
-            gallery: true,
-            item: 1,
-            loop: true,
-            slideMargin: 0,
-            thumbItem: 9
-        });
-
-        // Center Modal Window
-        $('.modal').css({
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            margin: '-' + ($('.modal').height() / 2) + 'px 0 0 -' + ($('.modal').width() / 2) + 'px'
-        });
-
-        // Triggle
-        $('#triggle').click(function (e) {
-            e.preventDefault();
-            $('.modal').fadeIn(400);
-            $('.overlay').fadeIn(400);
-            slider.refresh();
-        });
-
-        // Close Modal Windwo
-        $('a.close').click(function (e) {
-            e.preventDefault();
-            $('.modal').fadeOut(800);
-            $('.overlay').fadeOut(800);
-        });
-
-    });
-
-    $('#lightSlider').lightSlider({
-        gallery: true,
-        item: 1,
-        loop: true,
-        slideMargin: 0,
-        thumbItem: 9
-    });
-
     // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
     var $grid = $('.grid').masonry({
